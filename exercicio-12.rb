79ed9c2371b32df3107194b9a9ed78d2c3f4c28a
Thayna Santana <88935936+ThaynaSantana@users.noreply.github.com>
2022-02-25 14:29:47 -0300

    jogo de par e impar feito ✔

--- exercicio-12.rb
+++ exercicio-12.rb
@@ -1,32 +1,33 @@
 #Brincadeira do ímpar ou par: escreva um programa que informe quem ganhou o “ímpar ou par”
 print "PAR OU IMPAR? : "
-jogador = gets.upcase.to_s
-#escolhendo par ou impar para o computador
+jogador = gets.chomp.upcase.to_s
 if jogador == "PAR" then
     pc = "IMPAR"
+    print "Digite o número PAR: "
+    jogador_numero = gets.to_i
+    pc_numero = rand(11)
+    if (jogador_numero + pc_numero) % 2 == 0 then
+        puts "Voce jogou como #{jogador} e VOCE GANHOU!!!"
+        puts "Voce jogou #{jogador_numero} e o computador jogou #{pc_numero}"
+        puts "O total deu #{jogador_numero+pc_numero}."
+    elsif (jogador_numero + pc_numero) % 2 > 0 then
+        puts "Voce jogou como #{jogador} e VOCE PERDEU SEU OTARIO!!!"
+        puts "Voce jogou #{jogador_numero} e o computador jogou #{pc_numero}"
+        puts "EU GANHEI HAHAHAHHA"
+        puts "O total deu #{jogador_numero+pc_numero}"
+    end
 elsif jogador == "IMPAR" then
     pc = "PAR"
-
-#recebendo o valor do jogador e um numero aleatorio para o computador
-print "O VALOR: "
-jogador_numero = gets.to_i
-pc_numero = rand(11)
-
-#Jogador PAR e resultado PAR
-if jogador == "PAR" and (jogador_numero + pc_numero) % 2 == 0 then
-    puts "Voce jogou como #{jogador} e VOCE GANHOU!!!"
-    puts "O total deu #{jogador_numero+pc_numero}."
-#Jogador PAR e resultado IMPAR
-elsif jogador == "PAR" and (jogador_numero + pc_numero) % 2 > 0 then
-    puts "Voce jogou como #{jogador} e VOCE PERDEU SEU OTARIO!!!"
-    puts "EU GANHEI HAHAHAHHA"
-    puts "O total deu #{jogador_numero+pc_numero}"
-#Jogador IMPAR e resultado PAR
-elsif jogador == "IMPAR" and (jogador_numero + pc_numero) % 2 == 0 then
-    puts "Voce jogou como #{jogador} e VOCE PERDEU KKKK!!!"
-    puts "O total deu #{jogador_numero+pc_numero}."
-#Jogador IMPAR e resultado IMPAR
-elsif jogador == "IMPAR" and (jogador_numero + pc_numero) % 2 > 0 then
-    puts "Voce jogou como #{jogador} e VOCE GANHOU!!!"
-    puts "O total deu #{jogador_numero+pc_numero}"
+    print "Digite um número IMPAR: "
+    jogador_numero = gets.to_i
+    pc_numero = rand(11)
+    if (jogador_numero + pc_numero) % 2 == 0 then
+        puts "Voce jogou como #{jogador} e VOCE PERDEU KKKK!!!"
+        puts "Voce jogou #{jogador_numero} e o computador jogou #{pc_numero}"
+        puts "O total deu #{jogador_numero+pc_numero}."
+    elsif (jogador_numero + pc_numero) % 2 > 0 then
+        puts "Voce jogou como #{jogador} e VOCE GANHOU!!!"
+        puts "Voce jogou #{jogador_numero} e o computador jogou #{pc_numero}"
+        puts "O total deu #{jogador_numero+pc_numero}"
+    end
 end
