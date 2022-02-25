7a58dec3e0233d918cbca560d8c762f481f960f0
Thayna Santana <88935936+ThaynaSantana@users.noreply.github.com>
2022-02-25 13:35:13 -0300

    exercicio 13 resolvido ;)

--- exercicio-13.rb
+++ exercicio-13.rb
@@ -1,11 +1,10 @@
 #Escreva um programa que leia um número real e faça o
 #arredondamento para inteiro. Se a parte fracionária for maior do que
 #0.5 o arredondamento deve ser feito para o o próximo inteiro. 
+puts "Se a parte fracionaria for maior que 0.5 o programa fará um arendodamento ;)"
 print "Digite um numero: "
-numberBruto = gets.to_s
-numberDividido = number.split(".")
-if numberDividido(1) > "0.5" then
-    numberDividido(0) += "1"
-    numberDividido(1) = "0"
-print "#{numberDividido}"
-end
+number = gets.chomp.to_f
+puts "Então: #{number.round(half: :up)}"
+# O metodo round faz com que arredonde um numero decimal que se a parte fracionaria for maior que 0.5
+# ele arredonda o valor inteiro e retira a parte fracionaria.
+# existem varios outros parametros que se pode dar ao metodo round. ;)
